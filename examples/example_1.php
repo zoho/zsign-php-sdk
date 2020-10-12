4b34fe2913a7713311ba8404f06ddb38fcc3ba67
kalai-zs <72127177+kalai-zs@users.noreply.github.com>
2020-10-12 12:29:23 +0530

    cleaning code

--- examples/example_1.php
+++ examples/example_1.php
@@ -22,10 +22,10 @@
 		**********/
 
 		$user = new OAuth( array(
-			OAuth::CLIENT_ID 	=> "1000.Y97OE5BFRXVJKY7OUAWFW6ZO4SUYVA",
-			OAuth::CLIENT_SECRET=> "5f60eb6a58ca63ffc37c14466104f29e1fdcd4707c",
-			OAuth::DC 			=> "COM",
-			OAuth::REFRESH_TOKEN=> "1000.c273d62198c4a66e1f9df5b4b54c4fcd.2556f1120e30f879db1bec4278b5a60c",
+			OAuth::CLIENT_ID => "",
+			OAuth::CLIENT_SECRET=> "",
+			OAuth::DC => "COM",
+			OAuth::REFRESH_TOKEN=> "",
 			// OAuth::ACCESS_TOKEN => "" // optional. If not set, will auto refresh for access token
 		) );
 
