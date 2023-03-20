b2828fc336af293cdebd27df336e4e90cb63bce5
vagee-4413 <vageeswaran.ms@zohocorp.com>
2023-03-20 14:57:47 +0530

    UpdateOauth file added

--- src/UpdateOAuth.php
+++ src/UpdateOAuth.php
@@ -4,8 +4,8 @@ class UpdateOAuth
 {
      public static function updateAccessToken ($accessToken,$expires_in)
      {
-        echo "The Access Token : ".$accessToken ."It will expires in ".$expires_in;
-        //uncomment UpdateOAuth::updateAccessToken() function in ApiClient.php 
+        //echo "The Access Token : ".$accessToken ."It will expires in ".$expires_in;
+        //uncomment UpdateOAuth::updateAccessToken() function in ApiClient.php
         //and write logic to store access token by this function
 
      }
